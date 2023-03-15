aed0affd43cbb97534ae2eeecd978113802bcd32
HovoYeghoyan07 <hovhannes.y1.g@tumo.org>
2023-03-15 11:49:15 +0300

    changes

---
+++ prog 2/Grass.js
@@ -0,0 +1,34 @@
+class Grass extends LivingCreature {
+    constructor(x, y) {
+        super(x,y)
+        this.multiply = 0
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+
+    }
+
+    
+    mul() {
+        this.multiply++;
+        var emptyCells = this.chooseCell(0);
+        var newCell = random(emptyCells);
+        console.log(emptyCells);
+        if (newCell && this.multiply >= 20) {
+            var newX = newCell[0];
+            var newY = newCell[1];
+            matrix[newY][newX] = 1;
+
+            var newGrass = new Grass(newX, newY);
+            grassArr.push(newGrass);
+            this.multiply = 0;
+        }
+    }
+}
---
+++ prog 2/Hum.js
@@ -0,0 +1,104 @@
+class Hum extends LivingCreature {
+    constructor(x, y) {
+       super(x,y)
+        this.energy = 8;
+        this.multiply = 0
+        this.directions = [];
+
+
+    }
+
+    
+   
+
+mul() {
+    this.multiply++;
+    var emptyCells = this.chooseCell(0);
+    var newCell = random(emptyCells);
+
+    console.log(emptyCells);
+    if (newCell && this.multiply >= 15) {
+        var newX = newCell[0];
+        var newY = newCell[1];
+        matrix[newY][newX] = 4;
+
+        var newPredator = new Hum(newX, newY);
+        HumArr.push(newPredator);
+        this.multiply = 0;
+    }
+}
+move() {
+    this.energy--
+    var emptyCells = this.chooseCell(0)
+    var newCell = emptyCells[Math.floor(Math.random() * emptyCells.length)]
+
+    if (newCell && this.energy >= 0) {
+        console.log(newCell)
+        var newX = newCell[0]
+        var newY = newCell[1]
+        matrix[newY][newX] = matrix[this.y][this.x]
+        matrix[this.y][this.x] = 0
+        this.x = newX
+        this.y = newY
+    }
+    else {
+        if (this.energy < 0) {
+            this.die()
+        }
+    }
+}
+move() {
+        this.energy--
+        var emptyCells = this.chooseCell(0)
+        var newCell = emptyCells[Math.floor(Math.random() * emptyCells.length)]
+
+        if (newCell && this.energy >= 0) {
+            console.log(newCell)
+            var newX = newCell[0]
+            var newY = newCell[1]
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+        }
+        else {
+            if (this.energy < 0) {
+                this.die()
+            }
+        }
+    }
+eat() {
+    var emptyCells = this.chooseCell(2)
+    var newCell = emptyCells[Math.floor(Math.random() * emptyCells.length)]
+
+    if (newCell) {
+        this.energy++
+        var newX = newCell[0]
+        var newY = newCell[1]
+
+        matrix[newY][newX] = matrix[this.y][this.x]
+        matrix[this.y][this.x] = 0
+        this.x = newX
+        this.y = newY
+        for (var i in grassEaterArr) {
+            if (newX == grassEaterArr[i].x && newY == grassEaterArr[i].y) {
+                grassEaterArr.splice(i, 1)
+                break
+            }
+        }
+    }
+    else {
+        this.move()     
+
+    }
+}
+die() {
+    matrix[this.y][this.x] = 0;
+    for (var i in humArr) {
+        if (this.x == humArr[i].x && this.y == humArr[i].y) {
+            humArr.splice(i, 1);
+            break;
+        }
+    }
+}
+}
